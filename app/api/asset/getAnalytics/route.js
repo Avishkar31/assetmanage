36302c22565b3f4163bb59421d96d6224f7a3913
srktheman000 <sohan.kinage000@gmail.com>
2024-10-01 20:18:57 +0530

    analytics api

--- app/api/asset/getAnalytics/route.js
+++ app/api/asset/getAnalytics/route.js
@@ -3,18 +3,22 @@ import Asset from "../../../../models/Asset";
 import dbConnect from "lib/dbConnect";
 
 export async function GET(req) {
-  const { searchParams } = req.nextUrl;
-  const serialNumber = searchParams.get("serialNumber");
   await dbConnect();
 
   try {
-    const existingAsset = await Asset.findOne({
-      serialNumber: String(serialNumber)
-    });
-    if (!existingAsset) {
-      return NextResponse.json({ error: "asset not found" }, { status: 404 });
-    }
-    return NextResponse.json(existingAsset, { status: 200 });
+    const inPoolCount = await Asset.countDocuments({ status: "Inpool" });
+    const newPurchaseCount = await Asset.countDocuments({ status: "New Purchase" });
+    const inactiveCount = await Asset.countDocuments({ status: "Inactive" });
+    const allAssetsCount = await Asset.countDocuments({});
+
+    const analytics = {
+      inPoolCount,
+      newPurchaseCount,
+      inactiveCount,
+      allAssetsCount
+    };
+
+    return NextResponse.json(analytics, { status: 200 });
   } catch (err) {
     console.log(err);
     return NextResponse.json({ error: err.message }, { status: 500 });
